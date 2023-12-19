274a38c5cd26d61bc16ccfffff4a7d3ce760f070
Sara PG <39550635+pullz6@users.noreply.github.com>
2023-12-19 14:41:29 +0000

    Add files via upload
    Final Upload, all analysis and visualisation completed !!

--- Titanic_EDA.py
+++ Titanic_EDA.py
@@ -54,24 +54,57 @@ df_lost = df[df.Survived==0]
 plt.figure()
 df_survived['Pclass'].value_counts().plot(kind='bar')
 plt.title('The Class Distribution of the Survivors')
-
-
 plt.figure()
 sns.heatmap(df.corr(), vmin=-1, vmax=1,annot=True,cmap="rocket_r")
 plt.show()
 
 # Read Images
 cv2.namedWindow("output", cv2.WINDOW_NORMAL)
-img = cv2.imread('titanic.jpeg')
+img = cv2.imread('titanic_edited.png')
 imS = cv2.resize(img, (960, 540))                # Resize image   
 
 # make a copy of the original image
 imageLine = imS.copy()
+
+#The lowest cabin level display 
+text = 'Passengers Lost:'  + str(cv_lost.iloc[1][0]) 
+text_2 = 'Passengers Survived:' + str(cv_survived.iloc[1][0])
 pointA = (254,468)
 pointB = (400,468)
-cv2.arrowedLine(imageLine, pointB, pointA, (0, 255, 0), thickness=2)
-cv2.imshow('Image Line', imageLine)
+org = (401,475)
+org_2 = (401,488)
+cv2.arrowedLine(imageLine, pointB, pointA, (169, 169, 169), thickness=2)
+cv2.putText(imageLine, text, org, fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (0,0,0),thickness=1)
+cv2.putText(imageLine, text_2, org_2, fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (0,0,0),thickness=1)
+
+
+#The middle cabin level display 
+text = 'Passengers Lost:'  + str(cv_lost.iloc[2][0]) 
+text_2 = 'Passengers Survived:' + str(cv_survived.iloc[2][0])
+pointB = (400,408)
+pointA = (257,408)
+org = (401,413)
+org_2 = (401,426)
+cv2.arrowedLine(imageLine, pointB, pointA, (169, 169, 169), thickness=2)
+cv2.putText(imageLine, text, org, fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (255,255,255),thickness=1)
+cv2.putText(imageLine, text_2, org_2, fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (255,255,255),thickness=1)
+
+
+#The upper cabin level display 
+text = 'Passengers Lost:'  + str(cv_lost.iloc[0][0]) 
+text_2 = 'Passengers Survived:' + str(cv_survived.iloc[0][0])
+pointB = (486,336)
+pointA = (378,336)
+org = (486,341)
+org_2 = (486,354)
+cv2.arrowedLine(imageLine, pointB, pointA, (169, 169, 169), thickness=2)
+cv2.putText(imageLine, text, org, fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (255,255,255),thickness=1)
+cv2.putText(imageLine, text_2, org_2, fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (255,255,255),thickness=1)
+
+
+cv2.imshow('Titanic EDA Analysis', imageLine)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 
 
